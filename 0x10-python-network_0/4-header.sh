973b76f954ef4294786bdf0ff180c304241b41fc
yabbby <ye.aba.net@gmail.com>
2024-07-23 14:20:09 +0300

    update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# sends a GET request to the URL
+#write a bash script that takes in a URL set sent get request to URL and displays the body of the response
+
 curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+
